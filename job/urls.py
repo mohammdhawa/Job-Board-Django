4070f487c012bfc828d2c36f03a8809e29f32949
hawa <ragab5434@gmail.com>
2022-10-04 21:15:39 +0300

    added front-end

--- job/urls.py
+++ job/urls.py
@@ -1,7 +1,7 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
+from . import views
 
-# urlpatterns = [
-#     path('', views.job_list, name='job_list'),
-#     path('<str:id>/', views.job_detail, name='job_detail'),
-# ]
+urlpatterns = [
+    path('', views.job_list, name='job_list'),
+    path('<str:id>/', views.job_detail, name='job_detail'),
+]
--- job/views.py
+++ job/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
+def job_list(request):
+    job_list = Job.objects.all()
+    
+    context = {'job_list': job_list}
+    return render(request, 'job/job_list.html', context)
+
+
+def job_detail(request, id):
+    job = Job.objects.get(id=id)
+    
+    context = {'job': job}
+    return render(request, 'job/job_detail.html', context)
