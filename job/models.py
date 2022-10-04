61205c5ec0b67c056e796864f898d88a7005182b
hawa <ragab5434@gmail.com>
2022-10-04 21:41:54 +0300

    Media files added

--- job/models.py
+++ job/models.py
@@ -15,6 +15,10 @@ class Job(models.Model):
         ('part time', 'part time'),
     )
     
+    def image_upload(instance, filename):
+        imagename , extension = filename.split(".")
+        return "jobs/%s.%s"%(instance.id, extension)
+    
     title = models.CharField(max_length=100) # column
     # location = 
     job_type = models.CharField(max_length=20, choices=JOBTYPE)
@@ -24,7 +28,7 @@ class Job(models.Model):
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='job/')
+    image = models.ImageField(upload_to=image_upload)
     
     def __str__(self):
         return str(self.title)
