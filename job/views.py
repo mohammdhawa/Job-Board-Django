531ad61b0fbd96bdc4dd95d7df695fa4c30178fa
hawa <ragab5434@gmail.com>
2022-10-05 16:32:06 +0300

    Editting pagination

--- job/views.py
+++ job/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import Job
+from django.core.paginator import Paginator
 
 # Create your views here.
 def job_list(request):
     jobs = Job.objects.all()
+    paginator = Paginator(jobs, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     
-    context = {'jobs': jobs}
+    context = {'jobs': page_obj}
     return render(request, 'job/job_list.html', context)
 
 
