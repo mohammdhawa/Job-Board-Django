7760ca90326a7c8095c505546790d0228a1c830b
hawa <ragab5434@gmail.com>
2022-10-06 21:26:13 +0300

    added profile and user forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignupForm, UserForm, ProfileForm
+from django.contrib.auth import authenticate, login
+from .models import Profile
+from django.urls import reverse
 
 # Create your views here.
 
 def signup(request):
+    
+    if request.method == 'POST':
+        form  = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, passwrod=password)
+            login(request, user)
+            return redirect('/accounts/profile')
+    else:
+        form = SignupForm()
+    
+    context = {'form': form}
+    return render(request, 'registration/signup.html', context)
+
+
+def profile(request):
+    profile = Profile.objects.get(user=request.user)
+    
+    context = {'profile': profile}
+    return render(request, 'accounts/profile.html', context)
+
+def profile_edit(request):
+    profile = Profile.objects.get(user=request.user)
+    
+    if request.method == 'POST':
+        userform = UserForm(request.POST, instance=request.user)
+        profileform = ProfileForm(request.POST, request.FILES, instance=profile)
+        if userform.is_valid() and profileform.is_valid():
+            userform.save()
+            myprofile = profileform.save(commit=False)
+            myprofile.user = request.user
+            myprofile.save()
+            return redirect(reverse('accounts:profile'))
+    else:
+        userform = UserForm(instance=request.user)
+        profileform = ProfileForm(instance=profile)
+    
+    context = {'userform': userform, 'profileform': profileform}
+    return render(request, 'accounts/profile_edit.html', context)
+
+def profile_delete(request):
     pass
