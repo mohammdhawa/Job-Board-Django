aeccfe1569d3d8c00e11eed15537fe0f0acee661
mohammdhawa <ragab5434@gmail.com>
2023-12-18 00:52:57 +0300

    add requirements.txt

--- project/settings.py
+++ project/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     
     'django_filters',
     'rest_framework',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -145,3 +146,9 @@ MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+GRAPH_MODELS = {
+  'all_applications': True,
+  'group_models': True,
+}
