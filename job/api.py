030b744f9a265f64110b8af47beec0741dc29050
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2025-01-25 22:46:30 +0300

    add ApplyJobSerializer and implement it in JobDetailAPI

--- job/api.py
+++ job/api.py
@@ -1,7 +1,11 @@
-from rest_framework import generics
-from .serializers import JobSerializer, JobDetailsSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from .serializers import JobSerializer, JobDetailsSerializer, ApplyJobSerializer
 from .models import Job
 from .job_custom_pagination import CustomPagination
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser, MultiPartParser, FormParser
 
 
 class JobListAPI(generics.ListAPIView):
@@ -10,6 +14,30 @@ class JobListAPI(generics.ListAPIView):
     pagination_class = CustomPagination
 
 
-class JobDetailAPI(generics.RetrieveAPIView):
-    queryset = Job.objects.all()
-    serializer_class = JobDetailsSerializer
+class JobDetailAPI(APIView):
+    parser_classes = [JSONParser, MultiPartParser, FormParser]  # Accept both JSON and form-data
+
+    def get(self, request, pk):
+        try:
+            job = Job.objects.get(id=pk)
+            serializer = JobDetailsSerializer(job)
+            return Response(serializer.data)
+        except Job.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request, pk):
+        try:
+            job = Job.objects.get(id=pk)
+        except Job.DoesNotExist:
+            return Response({"error": "Job not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ApplyJobSerializer(data=request.data, context={'job': job})
+        if serializer.is_valid():
+            serializer.save()
+            job_serializer = JobDetailsSerializer(job)  # Serialize the job for the response
+            return Response(job_serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
--- job/serializers.py
+++ job/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Job, Category
+from .models import Job, Category, Apply
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -22,3 +22,20 @@ class JobDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = '__all__'
+
+
+class ApplyJobSerializer(serializers.ModelSerializer):
+    # cv = serializers.FileField()  # Ensure this is a FileField
+    class Meta:
+        model = Apply
+        exclude = ['job']
+
+    def create(self, validated_data):
+        # Retrieve the job object from context
+        job = self.context.get('job')
+        if not job:
+            raise serializers.ValidationError("Job context is missing.")
+
+        # Set the job and create the ApplyJob instance
+        validated_data['job'] = job
+        return super().create(validated_data)
